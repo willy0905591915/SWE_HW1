a8ee5fec9a573722bcd62900f81690c1ef2d6976
Will <willy0972205402@gmail.com>
2024-10-14 13:42:25 -0400

    Add black and flake8 rules in repo

--- polls/models.py
+++ polls/models.py
@@ -2,19 +2,28 @@ from django.db import models
 import datetime
 from django.utils import timezone
 
-# Each model has a number of class variables, each of which represents a database field in the model
+
+# Each model has a number of class variables, each of which represents a database field in the model  # noqa: E50
 class Question(models.Model):
-    question_text = models.CharField(max_length=200) # Database will use these names as column names
-    pub_date = models.DateTimeField('date published')
+    question_text = models.CharField(
+        max_length=200
+    )  # Database will use these names as column names
+    pub_date = models.DateTimeField("date published")
+
     def __str__(self):
         return self.question_text
-    # It’s important to add __str__() methods to your models, not only for your own convenience when dealing with the interactive prompt, but also because objects’ representations are used throughout Django’s automatically-generated admin.
+
+    # It’s important to add __str__() methods to your models, not only for your own convenience when dealing with the interactive prompt, but also because objects’ representations are used throughout Django’s automatically-generated admin.  # noqa: E50
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    
+
+
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE) # The foreign key tells django each choice is related to a single question
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE
+    )  # The foreign key tells django each choice is related to a single question
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0) # default value is 0
+    votes = models.IntegerField(default=0)  # default value is 0
+
     def __str__(self):
         return self.choice_text
--- polls/tests.py
+++ polls/tests.py
@@ -3,37 +3,46 @@ from django.urls import reverse
 from .models import Question, Choice
 from django.utils import timezone
 
+
 class PollsViewTests(TestCase):
-    
+
     def setUp(self):
         """Set up a sample question for testing."""
-        self.question = Question.objects.create(question_text="Sample Question", pub_date=timezone.now())
-        self.choice1 = Choice.objects.create(question=self.question, choice_text="Option 1", votes=0)
-        self.choice2 = Choice.objects.create(question=self.question, choice_text="Option 2", votes=0)
+        self.question = Question.objects.create(
+            question_text="Sample Question", pub_date=timezone.now()
+        )
+        self.choice1 = Choice.objects.create(
+            question=self.question, choice_text="Option 1", votes=0
+        )
+        self.choice2 = Choice.objects.create(
+            question=self.question, choice_text="Option 2", votes=0
+        )
 
     def test_index_view_status_code(self):
         """Test that the index page loads successfully."""
-        response = self.client.get(reverse('polls:index'))
+        response = self.client.get(reverse("polls:index"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Sample Question")
-    
+
     def test_detail_view_status_code(self):
         """Test that the detail page for a poll loads successfully."""
-        response = self.client.get(reverse('polls:detail', args=[self.question.id]))
+        response = self.client.get(reverse("polls:detail", args=[self.question.id]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.question.question_text)
 
     def test_results_view_status_code(self):
         """Test that the results page loads successfully."""
-        response = self.client.get(reverse('polls:results', args=[self.question.id]))
+        response = self.client.get(reverse("polls:results", args=[self.question.id]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.question.question_text)
-    
+
     def test_vote_functionality(self):
         """Test the vote functionality."""
-        response = self.client.post(reverse('polls:vote', args=[self.question.id]), {
-            'choice': self.choice1.id
-        })
+        response = self.client.post(
+            reverse("polls:vote", args=[self.question.id]), {"choice": self.choice1.id}
+        )
         self.choice1.refresh_from_db()  # Refresh to get updated vote count
         self.assertEqual(self.choice1.votes, 1)
-        self.assertRedirects(response, reverse('polls:results', args=[self.question.id]))
+        self.assertRedirects(
+            response, reverse("polls:results", args=[self.question.id])
+        )
